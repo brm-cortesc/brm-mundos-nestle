fd01b3df5183926b359ed3417ffd1a2ee54d4cac
Cristian Cortés <cristian.cortes@brm.com.co>
2017-05-22 19:36:29 -0500

    sliders completed

--- publication/js/nestle.mundos.js
+++ publication/js/nestle.mundos.js
@@ -1,14 +1,45 @@
 jQuery(document).ready(function() {
-  var marcas;
-  console.log('tones el js');
 
   /* Config del carousel marcas */
+  var marcas, ppal;
   marcas = {
+    arrows: true,
     infinite: true,
-    slidesToShow: 11,
+    slidesToShow: 8,
     slidesToScroll: 1
   };
+  ppal = {
+    arrows: false,
+    slidesToScroll: 1,
+    autoplay: true,
+    fade: true,
+    autoplaySpeed: 5000
+  };
   jQuery('.container-carousel').slick(marcas);
+  jQuery('.container-slider .slider-categorias').slick(ppal);
+  jQuery('.menu a').hover((function() {
+
+    /*enter */
+    var categoria, pos;
+    categoria = jQuery(this).attr('data-category');
+    pos = jQuery(this).parent().index();
+
+    /* Pausamos rotador */
+    jQuery('.container-slider .slider-categorias').slick('slickPause');
+    jQuery('.container-slider .slide-intro-txt').addClass('unactive');
+
+    /* buscamos slide correspondiente y lo activamos */
+    jQuery('.container-slider .slider-categorias').slick('slickGoTo', pos);
+    setTimeout(function() {
+      jQuery('.container-slider .slide-' + categoria + ' .slide-hover').addClass('active');
+    }, 500);
+  }), function() {
+
+    /*out */
+    jQuery('.container-slider .slider-categorias').slick('slickPlay');
+    jQuery('.container-slider .slide-hover').removeClass('active');
+    jQuery('.container-slider .slide-intro-txt').removeClass('unactive');
+  });
 });
 
 //# sourceMappingURL=../maps/nestle.mundos.js.map
--- src/coffee/nestle.mundos.coffee
+++ src/coffee/nestle.mundos.coffee
@@ -1,15 +1,58 @@
 jQuery(document).ready ->
-	console.log 'tones el js'
 
 	### Config del carousel marcas ###
 	marcas =
+		arrows: true,
 		infinite: true,
-		slidesToShow: 11,
+		slidesToShow: 8,
 		slidesToScroll: 1
 
 
+	ppal =
+		arrows:false,
+		slidesToScroll: 1,
+		autoplay: true,
+		fade: true,
+		autoplaySpeed: 5000
+
 	jQuery('.container-carousel')
-		.slick marcas
+		.slick(marcas)
+
+	jQuery('.container-slider .slider-categorias')
+		.slick(ppal)
+
+	##Hover to show hover ###
+
+	jQuery('.menu a')
+		.hover (->
+			###enter ###
+			categoria = jQuery(@).attr('data-category')
+			pos = jQuery(@).parent().index()
+
+			### Pausamos rotador ###
+			jQuery('.container-slider .slider-categorias').slick 'slickPause'
+
+			jQuery('.container-slider .slide-intro-txt').addClass 'unactive'
+			
+			# jQuery('.container-slider .slide-'+categoria).addClass 'active'
+
+			### buscamos slide correspondiente y lo activamos ###
+			jQuery('.container-slider .slider-categorias').slick( 'slickGoTo', pos )
+			
+			setTimeout () ->
+				jQuery('.container-slider .slide-'+categoria+ ' .slide-hover').addClass 'active'
+				return
+			, 500
+
+			return
+
+		), ->
+			###out###
+			jQuery('.container-slider .slider-categorias').slick 'slickPlay'
+			jQuery('.container-slider .slide-hover').removeClass 'active'
+			jQuery('.container-slider .slide-intro-txt').removeClass 'unactive'
+			return
+
 
 	# Acá termina el ready
 	return
